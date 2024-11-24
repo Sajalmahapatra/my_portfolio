695cf7e5a8a3178a09106e4ebbe1135c16d6b93d
Sajalmahapatra <sajalmahapatra94@gmail.com>
2024-11-25 00:23:58 +0530

    fixed media query hook

--- src/Hooks/useSizeHook.jsx
+++ src/Hooks/useSizeHook.jsx
@@ -1,10 +1,11 @@
 import { useMediaQuery } from "@mantine/hooks";
 
 export const useSizeHook = () => {
-  const breakpoints = [576, 768, 992, 1200, 1408];
-  const [xs, sm, md, lg, xl] = breakpoints.map((bp) =>
-    useMediaQuery(`(min-width: ${bp}px)`)
-  );
+  const xs = useMediaQuery("(min-width: 576px)");
+  const sm = useMediaQuery("(min-width: 768px)");
+  const md = useMediaQuery("(min-width: 992px)");
+  const lg = useMediaQuery("(min-width: 1200px)");
+  const xl = useMediaQuery("(min-width: 1408px)");
 
   return { xs, sm, md, lg, xl };
 };
